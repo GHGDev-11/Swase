3bdb2f371ebe7f4f6682e85ce25cf0d1b6568f9c
GHGDev-11 <67170374+GHGDev-11@users.noreply.github.com>
2020-09-27 12:48:13 +0200

    Update Switch.py

--- Switch.py
+++ Switch.py
@@ -2,14 +2,11 @@ class Switch:
     def __init__(self):
         self.switchList = {}
         self.inputCMD = ''
-    
     def AddToSwitch(self, Input, Outcome):
         self.switchList[Input] = Outcome
         self.inputCMD += f'\n{Input}'
-    
     def PrintSwitch(self):
         print(self.switchList)
-    
     def LastSwitch(self, Input, Outcome, Else, Bind):
         self.switchList[Input] = Outcome
         self.inputCMD += f'\n{Input}'
